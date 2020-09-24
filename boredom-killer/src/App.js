6bfef965cc54deb992b583051d30923afb901372
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2020-09-23 19:00:27 -0700

    Added react to app

---
+++ boredom-killer/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* top  */}
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <h1>
+        Quarantine Activity Generator
+    </h1>
+
+    Headline
+      <br></br>
+      <br></br>
+
+    Random Image
+      <br></br>
+      <br></br>
+
+    Activity button
+
+      {/* bottom  */}
+    </div>
+  );
+}
+
+export default App;
